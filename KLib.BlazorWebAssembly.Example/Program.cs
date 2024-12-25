a841133f884ba13f0a22b13772f1c3c869c4b8df
MOrtali <marco.ortali@live.it>
2024-12-25 18:47:04 +0100

    Refactor of the project. Added example library. Move of the projects.

---
+++ KLib.BlazorWebAssembly.Example/Program.cs
@@ -0,0 +1,27 @@
+using KLib;
+using KLib.BlazorWebAssembly.Example.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddKLib();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
